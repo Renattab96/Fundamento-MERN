f43636e04eaccb9c61985556328e4f385ee1c678
Renatta Benitez <renattabenitez96@gmail.com>
2022-12-25 17:16:11 -0300

    Tarea Opcional de es hora de duelo

--- Javascript Spanish/Es hora de Duelo (Opcional)/dueloCarta.js
+++ Javascript Spanish/Es hora de Duelo (Opcional)/dueloCarta.js
@@ -1,3 +1,4 @@
+//ESTA ES UNA CARTA MAESTRA DE LA QUE HEREDAN LAS DEMAS CARTAS 
 class Card{
     constructor(nombre , costo){
         this.nombre= nombre;
@@ -5,6 +6,7 @@ class Card{
 
     }
 }
+//CARTA UNIDAD
 class Unidad extends Card{
     constructor(nombre, costo, poder ,resilencia ){
         super(nombre,costo);
@@ -12,45 +14,70 @@ class Unidad extends Card{
         this.resilencia= resilencia;
     }
 
-//Clase maestra de la que hereda las demas clases
+    //Clase maestra de la que hereda las demas clases
     // atacar(target){
     //     if(target instanceof Unidad )
     // }
+
     mostrar_Informacion(){
-        console.log(`El Ninja es  ${this.nombre} y tiene ${this.poder}`)
+        console.log(`El Ninja ${this.nombre} su poder   ${this.poder} resiliencia ${this.resilencia}`)
     }
 }
 
-class Efecto extends Card{
+    class Efecto extends Card{
     constructor ( nombre, costo, texto,stat,magnitud){
     super(nombre,costo);
     this.texto= texto;
     this.stat= stat;
     this.magnitud =magnitud;
     }
-    play(target){
-        if (target instanceof  Unidad ){
-            // console.log("que es el target ")
-            console.log(target.resilencia, "Que es target")
-            target.resilencia = target.resilencia + this.magnitud
 
+    play(target){
+        if (target instanceof Unidad && this.nombre =="Algoritmo Duro "){
+
+            console.log(this.nombre, "Que efecto estoy usando ")
+            target.resilencia = target.resilencia + this.magnitud;
+            
+        }else if(target instanceof Unidad && this.nombre=="Rechazo de Promesa"){
+            target.resilencia = target.resilencia + this.magnitud;
+
+        }else if (target instanceof Unidad && this.nombre =="Programacion en Pareja"){
+            target.poder = target.poder + this.magnitud;
         }else{
-            throw new Error("EL OBJETIVO DEBE VENIR DE LA UNIDAD ");
+            //throw new Error("El Objetivo debe venir de las Tarjetas");
         }
     }
     
 }
-//Efectos
+//EFECTOS
 
-let Algoritmo_duro= new Efecto("",2,"")
-let rechazo_de_promesa =new Efecto("",3,"")
-let programacion_pareja = new Efecto("",4,"")
-//turno 1
- let ninja_cinturon_rojo= new Unidad("Ninja cinturon rojo", 3, 3, 4)
-  ninja_cinturon_rojo.mostrar_Informacion()
-  Algoritmo_duro.play(ninja_cinturon_rojo)
- //turno 2
- let ninja_cinturon_negro= new Unidad("Ninja cinturon negro", 5, 6, 4)
- ninja_cinturon_negro.mostrar_Informacion()
-  Algoritmo_duro.play(ninja_cinturon_negro)
-//  console.log(this.nombre)
+let Algoritmo_duro= new Efecto("Algoritmo Duro ",
+                                2,
+                                " Aumentar la resitencia del objetivo en 2",
+                                'Resiliencia',
+                                4)
+let rechazo_de_promesa =new Efecto('Rechazo de Promesa',
+                                    1,
+                                    "Reducir la resitencia del objetivo en 2",
+                                    "resiliencia",
+                                    3)
+let programacion_pareja = new Efecto ('Programacion en Pareja ',
+                                    3,
+                                    "Aumentar el poder del objetivo en 2",
+                                    "Poder",
+                                    3)
+
+    //TURNO 1
+    let ninja_cinturon_rojo= new Unidad(" cinturon rojo", 3, 3, 4)
+    Algoritmo_duro.play(ninja_cinturon_rojo)
+    ninja_cinturon_rojo.mostrar_Informacion()
+
+    //TURNO 2
+    let ninja_cinturon_negro= new Unidad(" cinturon negro", 5, 6, 4)
+    Algoritmo_duro.play(ninja_cinturon_negro)
+    ninja_cinturon_negro.mostrar_Informacion()
+
+
+    //TURNO 3
+    programacion_pareja.play(ninja_cinturon_rojo)
+    ninja_cinturon_rojo.mostrar_Informacion()
