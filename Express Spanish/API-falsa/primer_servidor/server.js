74d697777d15b713c032d7fd4b5c88e8a4d6f487
Renatta Benitez <58010986+Renattab96@users.noreply.github.com>
2023-01-26 15:10:26 -0300

    Faker-API
    Se aplico en el proyecto Nodemon, Express y el uso de Faker para simular una api  con datos aleatorios

---
+++ Express Spanish/API-falsa/primer_servidor/server.js
@@ -0,0 +1,77 @@
+// faker api
+const {faker} = require ('@faker-js/faker')
+
+const Usuario =()=>{
+    return{
+        _id:faker.datatype.uuid(),
+        nombre:faker.name.firstName(),
+        apellido:faker.name.lastName(),
+        email:faker.internet.email(),
+        password: faker.internet.password(),
+        telefono:faker.phone.number(),
+
+    }
+}
+const empresa =()=>{
+    return{
+        _id:faker.datatype.uuid(),
+        nombre:faker.name.firstName(),
+        direccion:
+        {
+            calle:faker.address.street(),
+            ciudad:faker.address.city(),
+            departamento:faker.address.state(),
+            codigo_postal:faker.address.zipCode(),
+            pais:faker.address.country(),
+            
+        }
+
+    }
+}
+
+// llama al recurso de Express y el puerto 
+
+const express = require ('express')
+const app =  express()
+const PORT = 8000;
+
+app.listen(PORT , ()=>{
+    console.log(`Server Corriendo  ${PORT}`)
+})
+
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+
+//Creacion de Ruta de Lectura --Get
+
+app.get ('/api/user/new',(req, res)=>{
+    const usuarios = Usuario()
+    res.json({usuario:usuarios})
+    
+})
+
+
+app.get ('/api/companies/new',(req, res)=>{
+    const empresas = empresa()
+    res.json({empresa: empresas})
+    
+})
+
+app.get ('/api/user/company',(req, res)=>{
+    const empresas = empresa()
+    const usuarios = Usuario()
+
+    res.json({usuario:usuarios,empresa: empresas})
+    
+})
+// Creacion de ruta Guarda POST
+
+app.post('/crearusuario',(req, res)=>{
+    console.log(req.body)
+    res.json(req.body)
+
+})
